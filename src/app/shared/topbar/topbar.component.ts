05169600d299977bac1861a63151ba99c62cf3b6
Marco Pereira <marcocsp@hotmail.com>
2021-03-27 03:33:35 +0000

    Created a topbar component

---
+++ src/app/shared/topbar/topbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar',
+  templateUrl: './topbar.component.html',
+  styleUrls: ['./topbar.component.css'],
+})
+export class TopbarComponent implements OnInit {
+  @Input() navbarItems;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
