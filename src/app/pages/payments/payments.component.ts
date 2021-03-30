b6d3b9c2e4fd4532c1b4e04f3e3b8c94e43fccbd
Marco Pereira <marcocsp@hotmail.com>
2021-03-30 13:49:45 +0100

    Created PaymentManager service to handle payments. Refactored inputs and labels

--- src/app/pages/payments/payments.component.ts
+++ src/app/pages/payments/payments.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { CodeManagerService } from 'src/app/shared/code-manager/code-manager.service';
 
 @Component({
@@ -6,8 +7,35 @@ import { CodeManagerService } from 'src/app/shared/code-manager/code-manager.ser
   templateUrl: './payments.component.html',
   styleUrls: ['./payments.component.css'],
 })
-export class PaymentsComponent implements OnInit {
-  constructor(private _codeManager: CodeManagerService) {}
+export class PaymentsComponent implements OnDestroy {
+  private _subscription: Subscription = new Subscription();
+  matrixChars: Array<string>;
+  code: string;
+  status: boolean;
 
-  ngOnInit(): void {}
+  constructor(private _codeManager: CodeManagerService) {
+    this._subscribeCodeGenerationStatus();
+    this._subscribeMatrix();
+  }
+
+  ngOnDestroy() {
+    this._subscription.unsubscribe();
+  }
+
+  private _subscribeMatrix(): void {
+    this._subscription.add(
+      this._codeManager.generatedCodeObservable$.subscribe((generatedCode) => {
+        this.matrixChars = generatedCode.referenceMatrix.characters;
+        this.code = generatedCode.code;
+      })
+    );
+  }
+
+  private _subscribeCodeGenerationStatus(): void {
+    this._subscription.add(
+      this._codeManager.codeGenerationStatusObservable$.subscribe((x) => {
+        this.status = x;
+      })
+    );
+  }
 }
