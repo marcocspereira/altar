d4172fc0894b676f388aed35dc4a7cadd2ce745b
Marco Pereira <marcocsp@hotmail.com>
2021-04-02 15:24:59 +0100

    AppComponent ensures that sessionStorage is cleared onDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   links = [
     { title: 'Generator', route: '/generator' },
     { title: 'Payments', route: '/payments' },
   ];
+
+  ngOnDestroy() {
+    sessionStorage.removeItem('paymentData');
+  }
 }
