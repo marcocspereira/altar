d0aac835fd7b90ac0f9cc030b0b4b36a19b40e2c
Marco Pereira <marcocsp@hotmail.com>
2021-04-02 15:31:56 +0100

    Implemented form to select a code, name and amount to add to payments list. Also implemented feedback to user and integration with PaymentManager service.

--- src/app/pages/payments/payments.component.ts
+++ src/app/pages/payments/payments.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnDestroy } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { CodeManagerService } from 'src/app/shared/code-manager/code-manager.service';
+import { PatternValidators } from 'src/app/shared/altar-input/pattern-validators/pattern-validators';
+import { PaymentManagerService } from './payment-manager/payment-manager.service';
+import { PaymentData } from 'src/app/pages/payments/payment-data.type';
+import { Matrix } from 'src/app/shared/matrix/matrix';
 
 @Component({
   selector: 'app-payments',
@@ -9,13 +14,66 @@ import { CodeManagerService } from 'src/app/shared/code-manager/code-manager.ser
 })
 export class PaymentsComponent implements OnDestroy {
   private _subscription: Subscription = new Subscription();
-  matrixChars: Array<string>;
+  matrix: Matrix;
   code: string;
   status: boolean;
+  paymentData: Array<PaymentData>;
+  paymentForm: FormGroup;
+  errorMessages: Array<string>;
+  checksumHighlighted: PaymentData;
 
-  constructor(private _codeManager: CodeManagerService) {
+  constructor(
+    private _codeManager: CodeManagerService,
+    private _paymentManager: PaymentManagerService
+  ) {
     this._subscribeCodeGenerationStatus();
     this._subscribeMatrix();
+    this._subscribePaymentManager();
+    this._paymentManager.getCodes();
+    this.paymentForm = this.createForm();
+  }
+
+  createForm() {
+    return new FormGroup(
+      {
+        name: new FormControl('', [
+          Validators.required,
+          PatternValidators.alphanumeric,
+        ]),
+        amount: new FormControl('', [
+          Validators.required,
+          PatternValidators.numeric,
+        ]),
+      },
+      { updateOn: 'change' }
+    );
+  }
+
+  addCode(): void {
+    const isInvalidForm = this._manageFormInvalidity();
+    if (isInvalidForm) {
+      return;
+    }
+    const dataCode: PaymentData = {
+      name: this.paymentForm.controls.name.value,
+      amount: this.paymentForm.controls.amount.value,
+      code: this.code,
+      matrix: this.matrix,
+    };
+    this._paymentManager.persistCode(dataCode);
+    this._paymentManager.getCodes();
+  }
+
+  destroyCodes(): void {
+    this._paymentManager.destroyCodes();
+  }
+
+  highlightChecksum(data: PaymentData): void {
+    this.checksumHighlighted = data;
+  }
+
+  hideChecksum() {
+    this.checksumHighlighted = undefined;
   }
 
   ngOnDestroy() {
@@ -25,7 +83,7 @@ export class PaymentsComponent implements OnDestroy {
   private _subscribeMatrix(): void {
     this._subscription.add(
       this._codeManager.generatedCodeObservable$.subscribe((generatedCode) => {
-        this.matrixChars = generatedCode.referenceMatrix.characters;
+        this.matrix = generatedCode.referenceMatrix;
         this.code = generatedCode.code;
       })
     );
@@ -37,5 +95,33 @@ export class PaymentsComponent implements OnDestroy {
         this.status = x;
       })
     );
+  }
+
+  private _subscribePaymentManager(): void {
+    this._subscription.add(
+      this._paymentManager.paymentDataObservable$.subscribe((x) => {
+        this.paymentData = x;
+      })
+    );
+  }
+
+  private _manageFormInvalidity(): boolean {
+    this.errorMessages = [];
+    if (!this.code) {
+      this.errorMessages.push('A code must be generated in Generator page');
+    }
+    if (this.paymentForm.controls.name.hasError('required')) {
+      this.errorMessages.push('Name is required');
+    }
+    if (this.paymentForm.controls.name.hasError('invalidAlphanumeric')) {
+      this.errorMessages.push('Name must be alphanumeric');
+    }
+    if (this.paymentForm.controls.amount.hasError('required')) {
+      this.errorMessages.push('Amount is required');
+    }
+    if (this.paymentForm.controls.amount.hasError('invalidNumeric')) {
+      this.errorMessages.push('Amount must be numeric');
+    }
+    return this.errorMessages.length > 0 ? true : false;
   }
 }
--- src/app/pages/payments/payments.module.ts
+++ src/app/pages/payments/payments.module.ts
@@ -1,13 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { AltarInputModule } from 'src/app/shared/altar-input/altar-input.module';
 import { CodeBoxModule } from 'src/app/shared/code-box/code-box.module';
 import { PaymentsComponent } from './payments.component';
-
 const routes: Routes = [{ path: '', component: PaymentsComponent }];
 
 @NgModule({
   declarations: [PaymentsComponent],
-  imports: [RouterModule.forChild(routes), CodeBoxModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    FormsModule,
+    CodeBoxModule,
+    AltarInputModule,
+  ],
   exports: [RouterModule],
 })
 export class PaymentsModule {}
